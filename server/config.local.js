eb3c938fff697a79d63ea9ff357e908d0acb01a6
Anton Tuyakhov <atuyakhov@gmail.com>
2017-01-07 22:39:32 +0200

    add env configuration

---
+++ server/config.local.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "host": process.env.APP_HOST || "0.0.0.0",
+    "port": process.env.APP_PORT || 3000,
+};
---
+++ server/datasources.local.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "mongodb": {
+        "host": process.env.MONGODB_HOST || "0.0.0.0",
+        "port": process.env.MONGODB_PORT || 27017,
+        "database": process.env.MONGODB_DATABASE_NAME || "tamarin",
+        "password": process.env.MONGODB_PASSWORD || "",
+        "user": process.env.MONGODB_USERNAME || ""
+    }
+};
